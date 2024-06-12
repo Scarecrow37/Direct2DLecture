92dfc3bcd34416886a1fe1bb059fa5290283f232
Dosuha <weqa123@naver.com>
2024-06-12 16:57:56 +0900

    Add Window class and integrate into project
    A new class, Window, has been created to manage window creation and behavior. The class includes methods to register, create, and show the window, as well as handle window procedures. It has been integrated into the Engine project.

---
+++ Engine/Window/Window.cpp
@@ -0,0 +1,65 @@
+﻿#include "Window.h"
+
+Window::Window(const HINSTANCE instanceHandle, const int showCommand, const LPCWSTR name, const SIZE size):
+    _windowHandle(nullptr),
+    _showCommand(showCommand),
+    _windowClass{
+        sizeof(WNDCLASSEX), CS_HREDRAW | CS_VREDRAW, DefaultWindowProcedure,
+        NULL, NULL, instanceHandle, LoadIcon(instanceHandle, IDI_APPLICATION),
+        LoadCursor(instanceHandle, IDC_ARROW), static_cast<HBRUSH>(GetStockObject(WHITE_BRUSH)),
+        nullptr, name, LoadIcon(instanceHandle, IDI_APPLICATION)
+    },
+    _size(size)
+{
+}
+
+void Window::Register() const
+{
+    RegisterClassEx(&_windowClass);
+}
+
+void Window::Create()
+{
+    RECT rect = {0, 0, _size.cx, _size.cy};
+    AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
+    _windowHandle = CreateWindow(_windowClass.lpszClassName, _windowClass.lpszClassName, WS_OVERLAPPEDWINDOW,
+                                 rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top, nullptr, nullptr,
+                                 _windowClass.hInstance, nullptr);
+}
+
+void Window::Show()
+{
+    ShowWindow(_windowHandle, _showCommand);
+    UpdateWindow(_windowHandle);
+}
+
+void Window::PlaceInCenterOfScreen(const HWND windowHandle)
+{
+    const int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+    const int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+    RECT rect;
+    GetClientRect(windowHandle, &rect);
+    AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
+    const int clientWidth = rect.right - rect.left;
+    const int clientHeight = rect.bottom - rect.top;
+    SetWindowPos(windowHandle, nullptr, screenWidth / 2 - clientWidth / 2, screenHeight / 2 - clientHeight / 2,
+                 clientWidth, clientHeight, NULL);
+}
+
+LRESULT Window::DefaultWindowProcedure(const HWND windowHandle, const UINT message, const WPARAM wParam,
+                                       const LPARAM lParam)
+{
+    // TODO Full screen, Event programming, Extra commands
+    switch (message)
+    {
+    case WM_CREATE:
+        PlaceInCenterOfScreen(windowHandle);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(windowHandle, message, wParam, lParam);
+    }
+    return 0;
+}
---
+++ Engine/Window/Window.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "../Framework.h"
+
+class Window
+{
+public:
+    Window(HINSTANCE instanceHandle,int showCommand, LPCWSTR name, SIZE size);
+    void Register() const;
+    void Create();
+    void Show();
+
+private:
+    static void PlaceInCenterOfScreen(HWND windowHandle);
+    static LRESULT DefaultWindowProcedure(HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam);
+    
+    HWND _windowHandle;
+    int _showCommand;
+    WNDCLASSEX _windowClass;
+    SIZE _size;
+};
