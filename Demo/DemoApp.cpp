45cc62c47df5d79a91ba02245589ba81063ac1e6
Dosuha <weqa123@naver.com>
2024-06-13 20:02:33 +0900

    Add Initialize and OnRender methods to DemoApp
    In DemoApp, two new methods, Initialize and OnRender, have been added. Initialization for Scale, Rotation, Translation, and BitmapFromFile are now handled in the Initialize method, while OnRender sets the WorldMatrix and DrawBitmap. These updates help structure rendering and initialization properties within the DemoApp.

--- Demo/DemoApp.cpp
+++ Demo/DemoApp.cpp
@@ -1,5 +1,31 @@
 ﻿#include "DemoApp.h"
+#include "../Engine/Renderer/D2DRenderer.h"
+#include <d2d1.h>
 
-DemoApp::DemoApp(const HINSTANCE instanceHandle, const int showCommand, const wchar_t* const gameName): GameApp(instanceHandle, showCommand, gameName)
+D2D_MATRIX_3X2_F Scale = D2D1::Matrix3x2F::Identity();
+D2D_MATRIX_3X2_F Rotation = D2D1::Matrix3x2F::Identity();
+D2D_MATRIX_3X2_F Translate = D2D1::Matrix3x2F::Identity();
+D2D_MATRIX_3X2_F WorldMatrix = D2D1::Matrix3x2F::Identity();
+ID2D1Bitmap* Bitmap = nullptr;
+
+DemoApp::DemoApp(const HINSTANCE instanceHandle, const int showCommand, const wchar_t* const gameName): GameApp(
+    instanceHandle, showCommand, gameName)
 {
+}
+
+void DemoApp::Initialize(const bool isRelease)
+{
+    GameApp::Initialize(isRelease);
+    Scale = D2D1::Matrix3x2F::Scale(1.f, 1.f);
+    Rotation = D2D1::Matrix3x2F::Rotation(0.f);
+    Translate = D2D1::Matrix3x2F::Translation(0.f, 0.f);
+    _renderer->BitmapFromFile(L"../Resources/Images/atk_1.png", &Bitmap);
+}
+
+void DemoApp::OnRender(const D2DRenderer* renderer)
+{
+    GameApp::OnRender(renderer);
+    WorldMatrix = Scale * Rotation * Translate;
+    renderer->SetTransform(WorldMatrix);
+    renderer->DrawBitmap(Bitmap);
 }
--- Demo/DemoApp.h
+++ Demo/DemoApp.h
@@ -5,4 +5,8 @@ class DemoApp final : public GameApp
 {
 public:
     DemoApp(HINSTANCE instanceHandle, int showCommand, const wchar_t* gameName);
+
+    void Initialize(bool isRelease = true) override;
+
+    void OnRender(const D2DRenderer* renderer) override;
 };
