82ab496d5cf8655a8626455d06ab67a2942f62eb
Dosuha <weqa123@naver.com>
2024-06-13 11:29:00 +0900

    Refactor window initialization in Test.cpp
    The recent changes in Test.cpp include a simplification of the window initialization process. The previous three-method sequence (Register, Create, Show) is now encapsulated into a single Initialize method. This makes the code more readable and easier to maintain.

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -8,9 +8,7 @@ int APIENTRY wWinMain(_In_ const HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
     Window window(hInstance, nShowCmd, L"Hello, World!", {1920, 1080});
-    window.Register();
-    window.Create();
-    window.Show();
+    window.Initialize();
     while (true)
     {
     }
