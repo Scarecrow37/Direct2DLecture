2d3604fe1b5d7ee6a0eebe02ae5e8506ae0cc8df
Dosuha <weqa123@naver.com>
2024-06-13 11:28:48 +0900

    Update GameApp to include D2DRenderer and initialization flag
    In the GameApp class, a D2DRenderer instance and an initialization flag were added. The Initialize method has been modified to accept a 'isRelease' parameter and initializes the Window and Renderer instances in a try-catch block. If initialization fails, the exception message is logged and the initialization flag is set to false.

--- Engine/GameApp.cpp
+++ Engine/GameApp.cpp
@@ -2,9 +2,11 @@
 
 #include "Logger/Logger.h"
 #include "Window/Window.h"
+#include "Renderer/D2DRenderer.h"
 
 GameApp::GameApp(const HINSTANCE instanceHandle, const int showCommand, const wchar_t* gameName):
-    _window(new Window(instanceHandle, showCommand, gameName, {1920, 1080}))
+    _window(new Window(instanceHandle, showCommand, gameName, {1920, 1080})), _renderer(new D2DRenderer),
+    _isInitialized(false)
 {
 }
 
@@ -13,12 +15,20 @@ GameApp::~GameApp()
     delete _window;
 }
 
-void GameApp::Initialize()
+void GameApp::Initialize(const bool isRelease)
 {
-    Logger::Initialize();
-    _window->Register();
-    _window->Create();
-    _window->Show();
+    Logger::Initialize(!isRelease);
+    try
+    {
+        _window->Initialize();
+        _renderer->Initialize(_window->GetHandle(), _window->GetWidth(), _window->GetHeight());
+        _isInitialized = true;
+    }
+    catch (std::exception& exception)
+    {
+        Logger::Log(Logger::Level::Error, exception.what());
+        _isInitialized = false;
+    }
 }
 
 void GameApp::Run()
--- Engine/GameApp.h
+++ Engine/GameApp.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 #include "Framework.h"
 
+class D2DRenderer;
 class Window;
 
 class GameApp
@@ -8,9 +9,13 @@ class GameApp
 public:
    GameApp(HINSTANCE instanceHandle, int showCommand, const wchar_t* gameName);
    virtual ~GameApp();
-   virtual void Initialize();
+   virtual void Initialize(bool isRelease = true);
    virtual void Run();
 
 protected:
    Window* _window;
+   D2DRenderer* _renderer;
+
+private:
+   bool _isInitialized;
 };
