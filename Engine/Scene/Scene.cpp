1867b634a47e43f53265760670fadc58fa098ab2
Dosuha <weqa123@naver.com>
2024-06-17 12:32:01 +0900

    Add new Scene class
    A new Scene class was added that includes methods for setting and getting parameters such as scale, rotation, and translation. Additionally, the transformation matrix and world transformation matrix for the scene can be updated and accessed. This class was included to the project as well.

---
+++ Engine/Scene/Scene.cpp
@@ -0,0 +1,62 @@
+﻿#include "Scene.h"
+
+Scene::Scene():
+    _parentScene(nullptr), _scale({1.f, 1.f}), _rotation(0.f), _translation({0.f, 0.f}),
+    _transform(D2D1::Matrix3x2F::Identity()), _worldTransform(D2D1::Matrix3x2F::Identity())
+{
+}
+
+void Scene::SetParentScene(const Scene* parentScene)
+{
+    _parentScene = parentScene;
+}
+
+Vector Scene::GetScale() const
+{
+    return _scale;
+}
+
+void Scene::SetScale(const Vector& scale)
+{
+    _scale = scale;
+}
+
+float Scene::GetRotation() const
+{
+    return _rotation;
+}
+
+void Scene::SetRotation(float rotation)
+{
+    _rotation = rotation;
+}
+
+Vector Scene::GetTranslation() const
+{
+    return _translation;
+}
+
+void Scene::SetTranslation(const Vector& translation)
+{
+    _translation = translation;
+}
+
+Matrix Scene::GetTransform() const
+{
+    return _transform;
+}
+
+Matrix Scene::GetWorldTransform() const
+{
+    return _worldTransform;
+}
+
+void Scene::UpdateTransform()
+{
+    const Matrix scaleMatrix = Matrix::Scale(_scale);
+    const Matrix rotationMatrix = Matrix::Rotation(_rotation);
+    const Matrix translationMatrix = Matrix::Translation(_translation);
+    _transform = scaleMatrix * rotationMatrix * translationMatrix;
+    if (_parentScene != nullptr) _worldTransform = _transform * _parentScene->_worldTransform;
+    else _worldTransform = _transform;
+}
---
+++ Engine/Scene/Scene.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "../Math/Matrix.h"
+#include "../Math/Vector.h"
+
+
+class Scene
+{
+public:
+    Scene();
+    virtual ~Scene() = default;
+    
+    void SetParentScene(const Scene* parentScene);
+    Vector GetScale() const;
+    void SetScale(const Vector& scale);
+    float GetRotation() const;
+    void SetRotation(float rotation);
+    Vector GetTranslation() const;
+    void SetTranslation(const Vector& translation);
+    Matrix GetTransform() const;
+    Matrix GetWorldTransform() const;
+
+protected:
+    void UpdateTransform();
+    const Scene* _parentScene;
+    Vector _scale;
+    float _rotation;
+    Vector _translation;
+    Matrix _transform;
+    Matrix _worldTransform;
+};
