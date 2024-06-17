f9a4d9d84f762799eb2ef755c54f9c8dccdb4ba3
Dosuha <weqa123@naver.com>
2024-06-17 12:31:45 +0900

    Add Matrix class with scale, rotation, translation methods
    Introduced a new Matrix class in the Engine/Math directory. It inherits from D2D1_MATRIX_3X2_F and includes static methods for scale, rotation, and translation transformations of vectors. This provides more efficient and functional matrix manipulation in the engine.

---
+++ Engine/Math/Matrix.cpp
@@ -0,0 +1,20 @@
+﻿#include "Matrix.h"
+
+Matrix::Matrix(D2D1_MATRIX_3X2_F matrix): D2D_MATRIX_3X2_F(matrix)
+{
+}
+
+Matrix Matrix::Scale(const Vector& size, const Vector& center)
+{
+    return D2D1::Matrix3x2F::Scale(size, center);
+}
+
+Matrix Matrix::Rotation(const float angle, const Vector& center)
+{
+    return D2D1::Matrix3x2F::Rotation(angle, center);
+}
+
+Matrix Matrix::Translation(const Vector& size)
+{
+    return D2D1::Matrix3x2F::Translation(size);
+}
---
+++ Engine/Math/Matrix.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include <d2d1.h>
+
+#include "Vector.h"
+
+struct Matrix : D2D1_MATRIX_3X2_F
+{
+    static Matrix Scale(const Vector& size, const Vector& center = Vector::Zero);
+    static Matrix Rotation(float angle, const Vector& center = Vector::Zero);
+    static Matrix Translation(const Vector& size);
+
+    Matrix(D2D1_MATRIX_3X2_F matrix);
+};
