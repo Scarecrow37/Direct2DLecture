e6db3b631c66ac649ca54b8422c70464593cbad0
Dosuha <weqa123@naver.com>
2024-06-17 12:31:31 +0900

    Add Vector class in Math module
    Created a new Vector class in the Math module. This Vector class is built off the D2D1_VECTOR_2F structure and includes conversion operators to D2D1_SIZE_F and D2D1_POINT_2F. Added a zero initialization for convenience.

---
+++ Engine/Math/Vector.cpp
@@ -0,0 +1,5 @@
+﻿#include "Vector.h"
+
+Vector::Vector(const float inX, const float inY): D2D_VECTOR_2F({inX, inY})
+{
+}
---
+++ Engine/Math/Vector.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <d2d1_1.h>
+
+struct Vector : D2D1_VECTOR_2F
+{
+    static constexpr Vector Zero = {0.f, 0.f};
+    Vector(float inX, float inY);
+
+    operator D2D1_SIZE_F() const
+    {
+        return D2D1::Size(x, y);
+    }
+
+    operator D2D1_POINT_2F() const
+    {
+        return D2D1::Point2F(x, y);
+    }
+};
