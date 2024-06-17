f6561879d8ae7fac9dca4fd8e331e46603602683
Dosuha <weqa123@naver.com>
2024-06-17 11:55:23 +0900

    Enable trace and error console streams
    The previously commented out code is now enabled, which opens file streams for both trace logs and error logs in the Console constructor. If there's a failure in opening the streams, a failure message is now logged to stderr.

--- Engine/Utilities/Console.cpp
+++ Engine/Utilities/Console.cpp
@@ -1,15 +1,18 @@
 ﻿#include "Console.h"
 
+#include <cstdio>
+#include <iostream>
+
 Console::Console()
 {
     const BOOL _ = AllocConsole(); // Open new Console
     
-    // FILE* traceConsoleStream; // Open file stream of console for trace log;
-    // errno_t error = freopen_s(&traceConsoleStream, "CONOUT$", "w", stdout);
-    // if (error != 0) std::cerr << "Failed to open trace log console stream!" << std::endl;
-    // FILE* errorConsoleStream; // Open file stream of console for error log;
-    // error = freopen_s(&errorConsoleStream, "CONOUT$", "w", stderr);
-    // if (error != 0) std::cerr << "Failed to open error log console stream!" << std::endl;
+    FILE* traceConsoleStream; // Open file stream of console for trace log;
+    errno_t error = freopen_s(&traceConsoleStream, "CONOUT$", "w", stdout);
+    if (error != 0) std::cerr << "Failed to open trace log console stream!" << std::endl;
+    FILE* errorConsoleStream; // Open file stream of console for error log;
+    error = freopen_s(&errorConsoleStream, "CONOUT$", "w", stderr);
+    if (error != 0) std::cerr << "Failed to open error log console stream!" << std::endl;
 }
 
 Console::~Console()
