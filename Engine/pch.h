d9e8bb2505dbcd75bb0797269585d79a767e62c2
Dosuha <weqa123@naver.com>
2024-06-18 10:51:21 +0900

    Add precompiled headers and update project file
    New precompiled headers, pch.h and pch.cpp, have been added to the project. These headers include common libraries and definitions that are used across the project. Correspondingly, the project file (Engine.vcxproj) and the project filter file (Engine.vcxproj.filters) have been updated to include these new files along with other miscellaneous updates, such as the inclusion of more files and changes to the precompiled header settings.

---
+++ Engine/pch.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#ifndef  PCH_H
+#define PCH_H
+
+#include "Framework.h"
+#include <d2d1_1.h>
+#include <d2d1_1helper.h>
+#include <dwrite.h>
+#include <wincodec.h>
+#include <comdef.h>
+#include <d2d1_1helper.h>
+#include <string>
+
+#endif
