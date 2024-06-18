bcd991aa66e5da7bdd23fe7e092d458743db5001
Dosuha <weqa123@naver.com>
2024-06-18 15:14:35 +0900

    Refactor project structure, delete SimpleEngine, add ILogger and Logger
    The SimpleEngine project file was deleted, and two new separate projects named ILogger and Logger were created. The ILogger interface and its included LogLevel enum file were added as new files. Logger's header and source files were added as well. This restructuring of the project files promotes a cleaner architecture.

---
+++ ILogger/ILogger.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include <string>
+
+enum class LogLevel;
+
+class ILogger
+{
+public:
+    ILogger(const ILogger& other) = default;
+    ILogger(ILogger&& other) noexcept = default;
+    ILogger& operator=(const ILogger& other) = default;
+    ILogger& operator=(ILogger&& other) noexcept = default;
+    virtual ~ILogger() = default;
+
+    virtual void Log(LogLevel level, const char* message) = 0;
+    virtual void Log(LogLevel level, const std::string& message) = 0;
+    virtual void Log(LogLevel level, const wchar_t* message) = 0;
+    virtual void Log(LogLevel level, const std::wstring& message) = 0;
+};
