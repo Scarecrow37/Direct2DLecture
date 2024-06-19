553415bb4b2d9430b647d57e96ed169c3dc3ec94
Dosuha <weqa123@naver.com>
2024-06-19 12:43:36 +0900

    Add LevelLogger class with logability feature
    This commit introduces the LevelLogger class to our Logger suite. It includes new methods for setting and checking the "logability" of a LevelLogger instance, enhancing our control over which logging levels should be active.

---
+++ Logger/LevelLogger.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "LevelLogger.h"
+
+LevelLogger::LevelLogger(): _logable(true)
+{
+}
+
+inline void LevelLogger::SetLogable(const bool isLogable)
+{
+    _logable = isLogable;
+}
+
+inline bool LevelLogger::IsLogable() const
+{
+    return _logable;
+}
---
+++ Logger/LevelLogger.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+class LevelLogger
+{
+public:
+    LevelLogger();
+
+    inline void SetLogable(bool isLogable);
+    inline bool IsLogable() const;
+
+private:
+    bool _logable;
+};
