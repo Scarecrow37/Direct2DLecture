d7555633e2bf8130701a7e98f59d97917963c651
Dosuha <weqa123@naver.com>
2024-06-19 12:44:08 +0900

    Implement ConsoleLevelLogger class
    The new ConsoleLevelLogger class has been added. This class inherits from the LevelLogger class and mainly handles console-specific logging. It opens file streams for tracing console and error logs, and also provides mechanisms for allocating and freeing the console.

---
+++ Logger/ConsoleLogger/ConsoleLevelLogger.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "ConsoleLevelLogger.h"
+
+ConsoleLevelLogger::ConsoleLevelLogger(const WORD foreground):
+    _foreground(foreground)
+{
+    AllocConsole(); // Open new Console
+    FILE* traceConsoleStream; // Open file stream of console for trace log;
+    freopen_s(&traceConsoleStream, "CONOUT$", "w", stdout);
+    FILE* errorConsoleStream; // Open file stream of console for error log;
+    freopen_s(&errorConsoleStream, "CONOUT$", "w", stderr);
+}
+
+ConsoleLevelLogger::~ConsoleLevelLogger()
+{
+    FreeConsole();
+}
---
+++ Logger/ConsoleLogger/ConsoleLevelLogger.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "../LevelLogger.h"
+
+class ConsoleLevelLogger : public LevelLogger
+{
+public:
+    explicit ConsoleLevelLogger(WORD foreground);
+    ConsoleLevelLogger(const ConsoleLevelLogger& other) = default;
+    ConsoleLevelLogger(ConsoleLevelLogger&& other) noexcept = default;
+    ConsoleLevelLogger& operator=(const ConsoleLevelLogger& other) = default;
+    ConsoleLevelLogger& operator=(ConsoleLevelLogger&& other) noexcept = default;
+    virtual ~ConsoleLevelLogger();
+
+protected:
+    WORD _foreground;
+};
