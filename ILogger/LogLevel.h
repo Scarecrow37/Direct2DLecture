9aa9710e863fae487b377d50a7e4de5f3ef5e290
Dosuha <weqa123@naver.com>
2024-06-19 12:40:55 +0900

    Remove redundant comments from LogLevel.h
    The commit removes unnecessary explanation comments in the LogLevel.h file. The deleted block of comments was mostly self-explanatory and redundant, hence it has been cleaned up to make the code clearer and more presentable.

--- ILogger/LogLevel.h
+++ ILogger/LogLevel.h
@@ -1,12 +1,5 @@
 #pragma once
 
-/**
- * @enum LogLevel
- * @brief The enumeration class LogLevel represents the different levels of log messages.
- *
- * The LogLevel enumeration class is used to specify the severity level of log messages.
- * The levels are Trace, Debug, Information, Warning, Error, and Fatal.
- */
 enum class LogLevel
 {
     Trace,
