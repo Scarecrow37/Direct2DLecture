c3867f4af772496d4f0b09753ea3d19884840bd6
Dosuha <weqa123@naver.com>
2024-06-19 12:43:16 +0900

    Add new header files for Logger module
    New `pch.cpp`, `TargetVersion.h`, `Framework.h`, and `pch.h` files have been added to the Logger module. These files include core dependencies and SDK headers required for the proper functioning of the Logger module. This lays down the groundwork for subsequent development and additions to this module.

---
+++ Logger/pch.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef PCH_H
+#define PCH_H
+
+#include "Framework.h"
+#include "../ILogger/LogLevel.h"
+#include "../ILogger/ILoggerMultibyte.h"
+#include "../ILogger/ILoggerUnicode.h"
+
+#endif
