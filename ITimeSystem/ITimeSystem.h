c42befc392ac35cd6d9449fdc56ac10d2eb69706
Dosuha <weqa123@naver.com>
2024-06-20 11:29:23 +0900

    Add ITimeSystem interface
    This commit introduces the ITimeSystem interface with initialization, update, and deltatime methods. The interface also includes default and special member functions, providing a blueprint for the time system component in the application.

---
+++ ITimeSystem/ITimeSystem.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+class ITimeSystem
+{
+public:
+    ITimeSystem() = default;
+    ITimeSystem(const ITimeSystem& other) = default;
+    ITimeSystem(ITimeSystem&& other) noexcept = default;
+    ITimeSystem& operator=(const ITimeSystem& other) = default;
+    ITimeSystem& operator=(ITimeSystem&& other) noexcept = default;
+    virtual ~ITimeSystem() = default;
+
+    virtual void Initialize() = 0;
+    virtual void Update() = 0;
+
+    virtual float GetDeltaTime() const = 0;
+};
